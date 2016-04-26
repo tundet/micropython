b4cbf20cc92dc440abdd1787c0e1de67f701d697
ryunde <ryunde.taba@gmail.com>
2016-04-26 12:55:33 +0300

    rrdtool updating graph

--- getmessage.py
+++ getmessage.py
@@ -8,52 +8,40 @@ port = serial.Serial('/dev/ttyAMA0', 115200, timeout = 30)
 def readmessage():
 	while True:
 		a = port.readline().decode('ascii').strip()
-		print(a)
-		takaisin = tuple(a.split(","))
-		if len(takaisin) > 0:
-			print(takaisin)
-			time.sleep(5)
+		if len(a) > 0:
+#			print(a)
+#			time.sleep(5)
+
+			motion = a.split(",")[0]
+
+			light = a.split(",")[1]
+
+			temp = a.split(",")[2]
+			adddata(motion, light, temp)
+			graphrrd(motion, light, temp)
+
 		else:
 			break;
-		purgemessage(str(takaisin))
-
-def purgemessage(takaisin):
-	motion = takaisin.split()[0]
-	motion = motion.replace(",", "")
-	motion = motion.replace("(", "")
-	motion = motion.replace("'", "")
-       
-	light = takaisin.split()[1]
-	light = light.replace(",", "")
-	light = light.replace("'", "")
-	
-	temp = takaisin.split()[2]
-	temp = temp.replace(",", "")
-	temp = temp.replace(")", "")
-	temp = temp.replace("'", "")
-	
-	adddata(motion, light, temp)
-	#graphrrd(motion, light, temp)
 
 def createrrd():
 	subprocess.call(['rrdtool', 'create', 'test.rrd', '--step', '5',
-	'DS:motion:GAUGE:120:0:1',
-	'DS:light:GAUGE:120:0:400',
+	'DS:motion:GAUGE:120:0:10',
+	'DS:light:GAUGE:120:0:40',
 	'DS:temp:GAUGE:120:0:30',
 	'RRA:MAX:0.5:1:120'])
 
 def adddata(motion, light, temp):
 	subprocess.call(['rrdtool', 'update', 'test.rrd',
-	'N:'+ motion + ':'+ light + ':'+ temp])
+	'N:'+ str(motion) + ':'+str(light) + ':'+str(temp)])
 
 def graphrrd(motion, ligh, temp):
 	subprocess.call(['rrdtool', 'graph', 'test.png', '-w', '1000', '-h', '400',
-	'--start', str(int(-86400)), '--end', str(int(now)),
+	'--start', '-400', '--end', 'now',
 	'DEF:motion=test.rrd:motion:MAX', 'LINE1:motion#ff1289:motion',
-	'DEF:light=test.rrd:light:MAX', 'LINE1:light#008003:light',
+	'DEF:light=test.rrd:light:MAX', 'LINE1:light#ffb400:light',
 	'DEF:temp=test.rrd:temp:MAX', 'LINE1:temp#129111:temp'])
 
-createrrd()
+#createrrd()
 readmessage()
 
 
