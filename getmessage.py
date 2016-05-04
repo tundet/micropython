762f47d45a594fae266c19d56e8a7428ac0437a9
mipa1 <mikael.paloranta@hotmail.com>
2016-05-04 13:20:25 +0300

    added door trigger value

--- getmessage.py
+++ getmessage.py
@@ -17,8 +17,10 @@ def readmessage():
 			light = a.split(",")[1]
 
 			temp = a.split(",")[2]
-			adddata(motion, light, temp)
-			graphrrd(motion, light, temp)
+
+			door = a.split(",")[3]
+			adddata(motion, light, temp, door)
+			graphrrd(motion, light, temp, door)
 
 		else:
 			break;
@@ -28,20 +30,23 @@ def createrrd():
 	'DS:motion:GAUGE:120:0:10',
 	'DS:light:GAUGE:120:0:40',
 	'DS:temp:GAUGE:120:0:30',
+	'DS:door:GAUGE:120:0:20',
 	'RRA:MAX:0.5:1:120'])
 
-def adddata(motion, light, temp):
+def adddata(motion, light, temp, door):
 	subprocess.call(['rrdtool', 'update', 'test.rrd',
-	'N:'+ str(motion) + ':'+str(light) + ':'+str(temp)])
+	'N:'+ str(motion) + ':'+str(light) + ':'+str(temp) + ':'+str(door)])
 
-def graphrrd(motion, ligh, temp):
+def graphrrd(motion, ligh, temp, door):
 	subprocess.call(['rrdtool', 'graph', 'test.png', '-w', '1000', '-h', '400',
 	'--start', '-400', '--end', 'now',
 	'DEF:motion=test.rrd:motion:MAX', 'LINE1:motion#ff1289:motion',
 	'DEF:light=test.rrd:light:MAX', 'LINE1:light#ffb400:light',
-	'DEF:temp=test.rrd:temp:MAX', 'LINE1:temp#129111:temp'])
+	'DEF:temp=test.rrd:temp:MAX', 'LINE1:temp#129111:temp',
+	'DEF:door=test.rrd:door:MAX', 'LINE1:door#00e3e0:door'])
 
 #createrrd()
 readmessage()
+
 
 
