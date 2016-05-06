aa8b7bfc5d8ef55cf6387b76ef6efbe6c3a62f73
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2016-05-06 10:59:13 +0300

    Remove sendemail duplicate

--- sendemail.py
+++ sendemail.py
@@ -26,32 +26,3 @@ msg.preamble = 'RRD Graafeja'
 s = smtplib.SMTP('smtp.metropolia.fi')
 s.send_message(msg)
 s.quit()
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-
-
-sender = 'wimmaweppa@topkek.fi'
-receiver = 'mikael.paloranta@metropolia.fi'
-
-graph = 'test.png'
-img_data = open(graph, 'rb').read()
-
-msg = MIMEMultipart()
-text = MIMEText("testi")
-msg.attach(text)
-
-image = MIMEImage(img_data)
-msg.attach(image)
-
-msg['Subject'] = 'RRD Graafeja'
-msg['From'] = sender
-msg['To'] = receiver
-msg.preamble = 'RRD Graafeja'
-
-
-s = smtplib.SMTP('smtp.metropolia.fi')
-s.send_message(msg)
-s.quit()
-
