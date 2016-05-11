1c90a74d6da14904c4b9b87cd8b2dbe9ba77c9c4
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2016-05-11 12:28:48 +0300

    Getmessage comments

--- getmessage.py
+++ getmessage.py
@@ -6,11 +6,14 @@ import sys
 port = serial.Serial('/dev/ttyAMA0', 115200, timeout = 30)
 
 def readmessage():
+	#Loops reading messages
 	while True:
+		#Define port for messages
 		a = port.readline().decode('ascii').strip()
+
+		#If message is longer than 0
 		if len(a) > 0:
-#			print(a)
-#			time.sleep(5)
+			#Split values into their own variables
 
 			motion = a.split(",")[0]
 
@@ -19,12 +22,15 @@ def readmessage():
 			temp = a.split(",")[2]
 
 			door = a.split(",")[3]
+
+			#Give values to adddat and graphrrd functions
 			adddata(motion, light, temp, door)
 			graphrrd(motion, light, temp, door)
 
 		else:
 			break;
 
+#Function for creating graph, only do this once
 def createrrd():
 	subprocess.call(['rrdtool', 'create', 'test.rrd', '--step', '5',
 	'DS:motion:GAUGE:120:0:10',
@@ -33,10 +39,12 @@ def createrrd():
 	'DS:door:GAUGE:120:0:20',
 	'RRA:MAX:0.5:1:120'])
 
+#Function for adding the values to graph
 def adddata(motion, light, temp, door):
 	subprocess.call(['rrdtool', 'update', 'test.rrd',
 	'N:'+ str(motion) + ':'+str(light) + ':'+str(temp) + ':'+str(door)])
 
+#Function for updating the graph with latest values
 def graphrrd(motion, ligh, temp, door):
 	subprocess.call(['rrdtool', 'graph', 'test.png', '-w', '1000', '-h', '400',
 	'--start', '-400', '--end', 'now',
